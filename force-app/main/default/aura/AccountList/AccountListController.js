062a8f3123fba03e3d9116c99a8b01ddfe814381
Bustamante Lopez <cbustamantel@deloitte.com>
2018-10-31 16:02:57 -0300

    Fixed files for map marker

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -4,11 +4,9 @@
         action.setCallback(this, function(a) {
             component.set("v.accounts", a.getReturnValue());
             var event = $A.get("e.c:AccountsLoaded");
-            event.setParams({
-                "accounts": a.getReturnValue()
-            });
+            event.setParams({"accounts": a.getReturnValue()});
             event.fire();
         });
     $A.enqueueAction(action);
     }
-})
+  })
